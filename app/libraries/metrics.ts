9cb057ac4ab6f1dcbd9b9367527f656df4b738c8
Darwin Grisales <darwingrisalesg@gmail.com>
2025-02-13 23:56:42 -0500

    refactor: add proxy pattern

--- app/libraries/metrics.ts
+++ app/libraries/metrics.ts
@@ -1,8 +1,22 @@
-import { Metrics } from '@aws-lambda-powertools/metrics'
+import { Metrics } from "@aws-lambda-powertools/metrics";
+import type { MetricResolution, MetricUnit } from "@aws-lambda-powertools/metrics/lib/cjs/types";
+import { POWERTOOLS_METRICS_NAMESPACE, POWERTOOLS_SERVICE_NAME } from "@domain/constants/constants";
 
-const metrics = new Metrics({
-  namespace: 'serverlessAirline',
-  serviceName: 'products'
-})
+export const metrics = new Metrics({
+  namespace: POWERTOOLS_METRICS_NAMESPACE,
+  serviceName: POWERTOOLS_SERVICE_NAME
+});
 
-export default metrics
+export class LambdaMetrics {
+  /**
+   *
+   * Params:
+   *     @Param name – The metric name
+   *   @Param unit – The metric unit, see | MetricUnit
+   *   @Param value – The metric value
+   *   @Param resolution – The metric resolution, see | MetricResolution
+   */
+  static addMetric(name: string, unit: MetricUnit, value: number, resolution?: MetricResolution): void {
+    return metrics.addMetric(name, unit, value, resolution);
+  }
+}
