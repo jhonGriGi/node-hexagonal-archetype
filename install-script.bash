2743b7a47fa6353fcb4be3bbc0a00590b23db121
Darwin Grisales <darwingrisalesg@gmail.com>
2025-04-25 16:33:23 -0500

    refactor: update install-script

--- install-script.bash
+++ install-script.bash
@@ -3,25 +3,53 @@
 # URL del arquetipo (quemada en el script)
 REPO_URL="https://github.com/jhonGriGi/node-hexagonal-archetype"
 
-# Si se pasa un argumento, usarlo como carpeta; si no, usar el actual
-TARGET_DIR="${1:-.}"
+echo "🔧 Carpeta de proyecto o puedes usar la actual usando (.)"
+read PROJECT_FOLDER
+PROJECT_FOLDER=${PROJECT_FOLDER:-.} # Usa '.' si el usuario no escribe nada
 
-if [ "$TARGET_DIR" != "." ]; then
-  echo "Creando directorio $TARGET_DIR..."
-  mkdir -p "$TARGET_DIR"
+if [ "$PROJECT_FOLDER" != "." ]; then
+  echo "Creando directorio $PROJECT_FOLDER..."
+  mkdir -p "$PROJECT_FOLDER"
+  cd "$PROJECT_FOLDER" || exit 1
 fi
 
-cd "$TARGET_DIR" || exit 1
+# Package Manager
+echo "🔧 Package Manager para el proyecto [npm, pnpm, yarn]"
+read PACKAGE_MANAGER
+
+# Validar package manager
+if [[ "$PACKAGE_MANAGER" != "npm" && "$PACKAGE_MANAGER" != "pnpm" && "$PACKAGE_MANAGER" != "yarn" ]]; then
+  echo "❌ Package manager inválido. Debe ser npm, pnpm o yarn. Saliendo..."
+  exit 1
+fi
+
+# Nuevo remote origin (opcional)
+echo "🔧 Nuevo remote origin de git (opcional, deja vacío si no quieres configurarlo)"
+read REMOTE_ORIGIN
+
+# Limpiar repositorio git actual si existe
+DEFAULT_GIT_DIR="./.git"
+if [ -d "$DEFAULT_GIT_DIR" ]; then
+  rm -rf "$DEFAULT_GIT_DIR"
+fi
 
 echo "Clonando repositorio en $(pwd)..."
 git clone "$REPO_URL" . 
 
-echo "Eliminando remote origin..."
+echo "Eliminando remote origin de arquetipo"
 git remote remove origin
 
-echo "Borrando historial de commits..."
+echo "Borrando historial de commits de arquetipo"
 rm -rf .git
 git init
+
+# Si el usuario definió un remote origin, lo añadimos
+if [ -n "$REMOTE_ORIGIN" ]; then
+  echo "Agregando nuevo remote origin: $REMOTE_ORIGIN"
+  git remote add origin "$REMOTE_ORIGIN"
+else
+  echo "No se configuró remote origin."
+fi
 
 echo "Instalando dependencias usando $PACKAGE_MANAGER..."
 case "$PACKAGE_MANAGER" in
@@ -34,11 +62,6 @@ case "$PACKAGE_MANAGER" in
   pnpm)
     pnpm install
     ;;
-  *)
-    echo "Error: Gestor de paquetes no reconocido: $PACKAGE_MANAGER"
-    echo "Debe ser uno de: npm, yarn, pnpm"
-    exit 1
-    ;;
 esac
 
-echo "Proyecto listo en $(pwd)."
+echo "✅ Proyecto listo en $(pwd)."
