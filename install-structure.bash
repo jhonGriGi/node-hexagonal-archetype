7909a4ce7b0fa414d4b59bb4200f26c8fd835086
Jhon Darwin Grisales Giraldo <darwingrisalesg@gmail.com>
2025-08-05 16:31:26 -0500

    feature: add api response builder file in install-structure.bash

--- install-structure.bash
+++ install-structure.bash
@@ -207,7 +207,7 @@ trim_trailing_whitespace = false
 EOF
 
 echo "📂 Creating folder structure for Clean Architecture..."
-mkdir -p app app/adapters app/domain app/entrypoints app/libraries events
+mkdir -p app app/adapters app/domain app/domain/builders app/entrypoints app/libraries events
 
 cat > ./app/libraries/logger.ts << 'EOF'
 import { Loggerfy } from 'loggerfy';
@@ -263,6 +263,77 @@ import safeJsonStringify from 'safe-json-stringify'
 export const safeStringify = (data: object) => {
   return safeJsonStringify(data, null, 2)
 }
+
+EOF
+
+cat > ./app/domain/builders/api-response-builder.ts << 'EOF'
+export interface LambdaApiResponse {
+    statusCode: number;
+    headers?: Record<string, string>;
+    body?: string;
+    cookies?: string[];
+    isBase64Encoded?: boolean;
+}
+
+class LambdaResponseBuilder {
+    private statusCode!: number;
+    private headers?: Record<string, string>;
+    private body?: string;
+    private cookies?: string[];
+    private isBase64Encoded?: boolean;
+
+    constructor() {}
+
+    static empty() {
+        return new LambdaResponseBuilder();
+    }
+
+    withBody<T extends object>(body: T): LambdaResponseBuilder {
+        this.body = JSON.stringify(body);
+
+        return this;
+    }
+
+    withStatusCode(statusCode: number): LambdaResponseBuilder {
+        this.statusCode = statusCode;
+        return this;
+    }
+
+    withHeaders(headers: Record<string, string>): LambdaResponseBuilder {
+        this.headers = { ...this.headers, ...headers };
+        return this;
+    }
+
+    addCookie(cookie: string): LambdaResponseBuilder {
+        if (this.cookies == null) {
+            this.cookies = [];
+        }
+        this.cookies.push(cookie);
+        return this;
+    }
+
+    setCookies(cookies: string[]): LambdaResponseBuilder {
+        this.cookies = cookies;
+        return this;
+    }
+
+    setBase64Encoding(isBase64Encoded: boolean): LambdaResponseBuilder {
+        this.isBase64Encoded = isBase64Encoded;
+        return this;
+    }
+
+    build(): LambdaApiResponse {
+        return {
+            statusCode: this.statusCode,
+            headers: this.headers,
+            body: this.body,
+            cookies: this.cookies,
+            isBase64Encoded: this.isBase64Encoded,
+        };
+    }
+}
+
+export default LambdaResponseBuilder;
 
 EOF
 
