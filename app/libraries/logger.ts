267c203295bf51cfb0134e34c1452a85f11a2cbd
Jhon Darwin Grisales Giraldo <darwingrisalesg@gmail.com>
2025-08-05 11:13:58 -0500

    style: update indent of file

--- app/libraries/logger.ts
+++ app/libraries/logger.ts
@@ -3,43 +3,42 @@ import { Loggerfy } from 'loggerfy';
 const logger = new Loggerfy();
 
 export interface LOGGER_PARAMS {
-  code: string;
-  message: string;
-  detail?: string;
-  metadata?: Record<string, string>;
+    code: string;
+    message: string;
+    detail?: string;
+    metadata?: Record<string, string>;
 }
 
 class LambdaLogger {
-  static info({ code, message, metadata, detail }: LOGGER_PARAMS): void {
-    logger
-      .info()
-      .setCode(code)
-      .setDetail(detail ?? 'lambda_logger')
-      .setMessage(message)
-      .setMetadata(metadata ?? {})
-      .write();
-  }
+    static info({ code, message, metadata, detail }: LOGGER_PARAMS): void {
+        logger
+            .info()
+            .setCode(code)
+            .setDetail(detail ?? 'lambda_logger')
+            .setMessage(message)
+            .setMetadata(metadata ?? {})
+            .write();
+    }
 
-  static warn({ code, message, metadata, detail }: LOGGER_PARAMS): void {
-    logger
-      .warn()
-      .setCode(code)
-      .setDetail(detail ?? 'lambda_logger')
-      .setMessage(message)
-      .setMetadata(metadata ?? {})
-      .write();
-  }
+    static warn({ code, message, metadata, detail }: LOGGER_PARAMS): void {
+        logger
+            .warn()
+            .setCode(code)
+            .setDetail(detail ?? 'lambda_logger')
+            .setMessage(message)
+            .setMetadata(metadata ?? {})
+            .write();
+    }
 
-  static error({ code, message, metadata, detail }: LOGGER_PARAMS): void {
-    logger
-      .error()
-      .setCode(code)
-      .setDetail(detail ?? 'lambda_logger')
-      .setMessage(message)
-      .setMetadata(metadata ?? {})
-      .write();
-  }
+    static error({ code, message, metadata, detail }: LOGGER_PARAMS): void {
+        logger
+            .error()
+            .setCode(code)
+            .setDetail(detail ?? 'lambda_logger')
+            .setMessage(message)
+            .setMetadata(metadata ?? {})
+            .write();
+    }
 }
 
-export default LambdaLogger
-
+export default LambdaLogger;
